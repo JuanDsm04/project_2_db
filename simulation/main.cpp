a4bdb0fcf553ababfe911479165a107ff56b3531
Víctor Pérez <vmpch1@outlook.com>
2025-04-13 23:47:47 -0600

    feat: finish implementing simulation

--- simulation/main.cpp
+++ simulation/main.cpp
@@ -1,28 +1,79 @@
 #include <iostream>
+#include <cstdio>
 #include <string>
 #include <thread>
 #include <vector>
+#include <chrono>
 #include <pqxx/pqxx>
 
-int users = 5;
-std::string conninfo = "host=db port=5432 dbname=sim user=postgres password=postgres";
+// Parámetros
+const int users = 30;
+const int seat_id = 4;
+const int event_id = 1;
+const std::string isolation = "READ COMMITTED";
+const std::string conninfo = "host=db port=5432 dbname=sim user=postgres password=postgres";
 
-void make_reservation(int id) {
-	pqxx::connection cx(conninfo);
+// Abre una conexión a la base de datos
+// Verifica si ya existe una reservación previa
+// Si no, realiza la reservación
+void make_reservation(int id, int user_id) {
+	try {
+		pqxx::connection cx(conninfo);
 
-	std::cout << "Attempting to make reservation from thread " << id << std::endl;
+		{
+			pqxx::nontransaction iso_config(cx);
+			iso_config.exec0("SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL " + isolation);
+		}
+
+		std::cout << "[Thread " << id << "] Attempting to make a reservation.\n";
+
+		pqxx::work tx(cx);
+		pqxx::result r;
+
+		r = tx.exec_params(
+            "SELECT 1 FROM reservations WHERE event_id = $1 AND seat_id = $2",
+            event_id, seat_id
+        );
+
+		if (!r.empty()) {
+            std::cout << "[Thread " << id << "] Seat is already taken.\n";
+            return;
+        }
+
+        tx.exec_params(
+            "INSERT INTO reservations (seat_id, event_id, user_id) VALUES ($1, $2, $3)",
+            seat_id, event_id, user_id
+        );
+
+		tx.commit();
+
+		std::cout << "[Thread " << id << "] Reservation successful.\n";
+
+	} catch (const pqxx::serialization_failure &e) {
+        std::cout << "[Thread " << id << "] Serialization failure" << std::endl;
+    }
 }
 
 int main() {
 	std::vector<std::thread> threads;
+	int user_id = 1;
+
+	// Ejecución de la simulación
+	auto start_time = std::chrono::high_resolution_clock::now();
 
 	for (int i = 0; i < users; i++) {
-		threads.push_back(std::thread(make_reservation, i));
+		threads.push_back(std::thread(make_reservation, i, user_id));
+		user_id++;
 	}
 
 	for (auto &th: threads) {
 		th.join();
 	}
+
+	auto end_time = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> ms = end_time - start_time;
+
+	std::cout << "Time: " << ms.count() << std::endl;
 
 	return 0;
 }
